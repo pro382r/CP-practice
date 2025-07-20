40fa6cc39667c9cb4f93a23ade9ce47bf752b63a
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-07-20 19:42:17 +0600

    Create Domino_piling.cpp

---
+++ Codeforces/Domino_piling.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/50/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, n, k=0, k1;
+    cin >> a >> n;
+    k=a*n;
+
+    k1=k/2;
+cout << k1;
+    return 0;
+}
