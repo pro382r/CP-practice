eecc821d61763da66b81c84f7dcfd555ff3d533b
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-07-31 00:03:18 +0600

    Create ArafatandHisFavoriteSnack.cpp

---
+++ Reguler-problem/ArafatandHisFavoriteSnack.cpp
@@ -0,0 +1,16 @@
+/*
+Arafat is a student of Green University. After a long day of coding and classes, he finally gets time to enjoy his favorite snack. Today, he's feeling happy and wants to tell everyone what he's going to eat! Can you help Arafat print his favorite snack?
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string ss;
+    getline(cin, ss);
+    cout << "Today, Arafat will eat " << ss << "!" << endl;
+    
+    return 0;
+}
