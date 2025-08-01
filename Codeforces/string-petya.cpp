93d6ae7480030c43ea2c0124cd7455a440b6827b
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-08-02 02:53:13 +0600

    Add files via upload

---
+++ Codeforces/string-petya.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for(int i = 0; i < s1.size(); i++) {
+        char c1 = tolower(s1[i]);
+        char c2 = tolower(s2[i]);
+        
+        if(c1 < c2) {
+            cout << "-1";
+            return 0;
+        } else if(c1 > c2) {
+            cout << "1";
+            return 0;
+        }
+    }
+
+    cout << "0";
+    return 0;
+}
