c2e3a255f3e7200a4ab6c261d8a4c68da6483b04
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-08-04 23:55:16 +0600

    Create The Secret Even Code.cpp

---
+++ Reguler-problem/The Secret Even Code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int P;
+    cin >> P;
+    
+    if (P == 2) {
+        cout << 2 << endl;
+    } else {
+        cout << P << endl;
+    }
+    
+    return 0;
+}
