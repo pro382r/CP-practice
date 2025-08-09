b955b0d4d2d28555aaaa4157a43d820a1524ee71
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-08-10 00:30:01 +0600

    Create Magic Multiplication.cpp

---
+++ Reguler-problem/Magic Multiplication.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    if (k == 0) {
+        cout << -1 << endl;
+    } else {
+        cout << 1 << endl;
+    }
+    return 0;
+}
