1b0422a9012a083d4c435199675b1e8faa217389
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-08-10 00:48:20 +0600

    Create 	(A + B) again-with-mod.cpp

---
+++ Reguler-problem/(A + B) again-with-mod.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a,b,m,k;
+        cin >> a>> b>> m;
+        
+        k = (a+b)%m;
+        cout << k;
+        
+    return 0;
+    }
+}
