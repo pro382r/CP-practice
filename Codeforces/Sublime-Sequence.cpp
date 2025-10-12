5fe31f15fa5994354600d9e52dc0f5d9b9ded9cb
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-10-13 05:57:15 +0600

    Create Sublime-Sequence.cpp

---
+++ Codeforces/Sublime-Sequence.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/contest/2148/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n,x,i;
+        
+        cin >> x >> n;
+        
+        if(n%2==0){
+            cout << 0 << endl;
+        }else cout << x << endl;
+        
+    }
+}
