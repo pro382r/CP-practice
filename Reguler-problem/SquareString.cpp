72b967801b4bcf45235af65d6a1fc295e80add7d
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-11-05 05:33:57 +0600

    90% done-Implement SquareString problem solution

---
+++ Reguler-problem/SquareString.cpp
@@ -0,0 +1,42 @@
+//https://codeforces.com/problemset/problem/1619/A
+// not 100%
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin>> t;
+
+  while(t--){
+    int i,j,k;
+
+    string as, b1, b2;
+    cin >> as;
+
+    int n = as.size();
+    
+    if(n%2==1)
+    {
+        cout << "NO" << endl;
+    }else if(n%2==0)
+    {
+        k = n/2;
+
+        for(i=0; i<k; i++)
+        {
+            b1 = as[i];
+        }
+
+        for(i=k; i<n; i++)
+        {
+            b2 = as[i];
+        }
+
+        if(b1 == b2){
+            cout << "YES" << endl;
+        }else cout << "NO" << endl;
+    }
+  }
+    return 0;
+}
