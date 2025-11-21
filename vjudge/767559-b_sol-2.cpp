d6e820d22c69693870f4e6377faeacb3489343c7
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-11-22 00:08:47 +0600

    Add solution for problem 767559-b

---
+++ vjudge/767559-b_sol-2.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+#define pb push_back
+
+void solve(){
+    int i,j,k,n,x,a,b,c,C=0, sum, sm=0;
+    
+    cin >> n>> k >> x>> a>> b>> c;
+
+    vector<int>ar(n);
+
+    ar[0] = x;
+    int p=x;
+
+    for(i=1; i<n; i++)
+    {
+        p = (a*p+b)%c;
+        ar[i] = p;
+    }
+
+    int kp = k;
+    sum=0;
+
+    for(i=0; i<k; i++)
+    {
+        sum += ar[i];
+    }
+
+    sm=sum;
+    for(i=k; i<n; i++)
+    {
+        sum = sum + ar[i] - ar[i-k];
+        sm ^= sum;
+    }
+
+    cout << sm << endl;
+
+}
+ 
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
