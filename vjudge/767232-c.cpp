94f66569b8ec567127e0cdcce018fc8592840e2b
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-11-26 00:01:44 +0600

    Add C++ program for DNA base pairing

---
+++ vjudge/767232-c.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+char a,b;
+
+cin>> a;
+
+if(a=='A'){
+    cout << "T";
+}else if(a=='T'){
+    cout << "A";
+}else if(a=='G'){
+    cout << "C";
+}else if(a=='C'){
+    cout << "G";
+}
+
+    cout << "\n\n\n";
+    return 0;
+}
