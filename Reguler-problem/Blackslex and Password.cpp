1129d48d511cba7acb0a7f62e9dc43a1f856333c
💜 Rihun 💊 <pro382gamer@gmail.com>
2026-01-15 00:25:14 +0600

    Implement solution for Codeforces problem A

---
+++ Reguler-problem/Blackslex and Password.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/contest/2179/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T=1;
+    cin >> T;
+
+    while(T--)
+    {
+    int n,k=0, x=0;
+    cin >> k>> x;
+    
+    cout << (k*x+1) << endl;
+
+    }
+
+    return 0;
+}
