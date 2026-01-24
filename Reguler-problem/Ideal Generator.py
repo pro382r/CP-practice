324ad7f9e4accd2e052f0b85a089453834bdeec4
💜 Rihun 💊 <pro382gamer@gmail.com>
2026-01-25 04:07:26 +0600

    Add Ideal Generator script for Codeforces problem

---
+++ Reguler-problem/Ideal Generator.py
@@ -0,0 +1,13 @@
+//https://codeforces.com/contest/2093/problem/A
+
+
+t=0; T=1
+T = int(input())
+while t<T:
+    n = int(input())
+
+    if (n%2 == 0):
+        print("NO")
+    else:
+        print("YES")
+    t += 1
